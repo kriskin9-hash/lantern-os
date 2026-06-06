509ad790d642e07d0818001b253bbc0844e75783
cdblasioli-gif <cdblasioli@gmail.com>
2026-06-06 16:57:07 -0400

    chore(gitignore): ignore csf_rust/target build artifacts

--- src/csf_rust/target/release/deps/cfg_if-53316245ccaae430.d
+++
@@ -1,7 +0,0 @@
-C:\Users\angela\Github\lantern-os\src\csf_rust\target\release\deps\cfg_if-53316245ccaae430.d: C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\angela\Github\lantern-os\src\csf_rust\target\release\deps\libcfg_if-53316245ccaae430.rlib: C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\angela\Github\lantern-os\src\csf_rust\target\release\deps\libcfg_if-53316245ccaae430.rmeta: C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
--- src/csf_rust/target/release/deps/unicode_ident-30d2f53d806547ee.d
+++
@@ -1,8 +0,0 @@
-C:\Users\angela\Github\lantern-os\src\csf_rust\target\release\deps\unicode_ident-30d2f53d806547ee.d: C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\angela\Github\lantern-os\src\csf_rust\target\release\deps\libunicode_ident-30d2f53d806547ee.rlib: C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\angela\Github\lantern-os\src\csf_rust\target\release\deps\libunicode_ident-30d2f53d806547ee.rmeta: C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
-C:\Users\angela\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
