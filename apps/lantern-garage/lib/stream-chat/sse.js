b75a5453bd93792c527804566359aef313c65faa
Alex Place <alex.place.7@gmail.com>
2026-06-06 22:46:06 -0400

    refactor(stream-chat): split sse helpers

---
+++ apps/lantern-garage/lib/stream-chat/sse.js
@@ -0,0 +1,36 @@
+// SSE response helpers for streaming dream chat.
+
+function writeStreamHeaders(res) {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream; charset=utf-8",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Access-Control-Allow-Origin": "*",
+    "X-Accel-Buffering": "no",
+  });
+}
+
+function writeData(res, payload) {
+  res.write(`data: ${JSON.stringify(payload)}\n\n`);
+}
+
+function sendToken(res, token) {
+  writeData(res, { type: "token", text: token });
+}
+
+function sendError(res, text) {
+  writeData(res, { type: "error", text });
+}
+
+function sendDone(res, source, extra = {}) {
+  writeData(res, { type: "done", source, ...extra });
+  res.end();
+}
+
+module.exports = {
+  writeStreamHeaders,
+  writeData,
+  sendToken,
+  sendError,
+  sendDone,
+};
